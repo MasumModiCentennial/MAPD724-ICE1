f7ba219ec1256b476a5a1c8973b2b2a9af32b0ed
masummodi <masum.modi@yobored.com>
2021-01-31 20:27:00 -0500

    Initial Commit

---
+++ ICE 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ICE 1
+//
+//  Created by Masum Modi on 2021-01-31.
+//  Copyright © 2021 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
